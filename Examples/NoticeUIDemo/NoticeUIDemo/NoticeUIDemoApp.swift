625bf95c05f34ff3dddb4af2e0053c54596828af
Sameer Nadaf <nsameernadaf@gmail.com>
2026-02-04 12:28:42 +0530

    Initialize the demo project

---
+++ Examples/NoticeUIDemo/NoticeUIDemo/NoticeUIDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoticeUIDemoApp.swift
+//  NoticeUIDemo
+//
+//  Created by Sameer Nadaf on 04/02/26.
+//
+
+import SwiftUI
+
+@main
+struct NoticeUIDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
