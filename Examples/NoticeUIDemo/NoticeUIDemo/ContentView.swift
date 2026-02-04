fe7a56b36bfac95cbab4856ee6496574e906ceaf
Sameer Nadaf <nsameernadaf@gmail.com>
2026-02-04 14:00:17 +0530

    Update NoticeUIDemo with expanded test cases and style examples

--- Examples/NoticeUIDemo/NoticeUIDemo/ContentView.swift
+++ Examples/NoticeUIDemo/NoticeUIDemo/ContentView.swift
@@ -6,15 +6,28 @@
 //
 
 import SwiftUI
+import NoticeUI
 
 struct ContentView: View {
+    
+    @State private var toast: Toast?
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(spacing: 20) {
+            Button("Show Sucess") {
+                toast = Toast(message: "Sucess toast shown", role: .success, placement: .top, duration: .short)
+            }
+            Button("Show Warning") {
+                toast = Toast(message: "Warning toast shown", role: .warning, placement: .bottom, duration: .long)
+            }
+            Button("Show Error") {
+                toast = Toast(message: "Error toast shown", role: .error, placement: .top, duration: .custom(1.0))
+            }
+            Button("Show Info") {
+                toast = Toast(message: "Info toast shown", role: .info, placement: .center, duration: .indefinite)
+            }
         }
+        .toast($toast, style: PaleToastStyle())
         .padding()
     }
 }
